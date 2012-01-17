4b7e4e70b99eab9907d75f02a337d4b4eee73c60
Varrun Ramani <darthsitius@gmail.com>
2012-01-17 11:35:42 -0500

    fixed bug in stree_search()

--- src/stree.c
+++ src/stree.c
@@ -15,56 +15,46 @@ void init_node(struct Node**);
 void stree_add_word(struct Node **,char *);
 int stree_search(struct Node **, char *);
 struct Node* path_exists(struct Node**, int);
-void alloc_node(struct Node**);
 void do_dfs(struct Node **,char *);
 int is_terminal_str(struct Node **);
-int stree_search_substr(struct Node **, char *);
 
 int main()
 {
     struct Node* root;
     struct Node* temp;
     int i = 0;
-    int *blah;
     char *str = "aabcddd$";
+    char *search_str = "bacd";
+
     // Initialize root
     init_node(&root);
+    printf("-Initialized root....\n");
+
     // Adding suffixes
     for(i=0;i<strlen(str)-1;i++){
-        printf("%s\n",(str+i));
+    //    printf("%s\n",(str+i));
         stree_add_word(&root,str+i);
-    }
-   // DFS to print out all strings
-    printf("Strings:\n");
-    do_dfs(&root,"");
+    } printf("-Added suffixes....\n");
+    
+    // DFS to print out all strings
+    //printf("Strings:\n");
+    //do_dfs(&root,"");
    
-   // Search for a string
-    if(stree_search_substr(&root,"dda"))
-        printf("Yes\n");
-    else printf("No\n");
+    // Search for a string
+    printf("-Searching for '%s'.....\n",search_str);
+    if(stree_search(&root,search_str))
+        printf("Yippie! Its there! :-) \n");
+    else printf("Bad Luck Bro! :-( \n");
+    
     return 0;
 }
 int is_terminal_str(struct Node **cnode)
 {
     if( (*cnode)->ptrs[get_i(EOS)] != NULL) return 1; else return 0;
 }
-int stree_search_substr(struct Node **root, char *substr)
-{
-    struct Node *temp = *root;
-    int flag = 0,i=0;
-    for(i=0;i<strlen(substr);i++){
-        if(temp->ptrs[get_i(substr[i])] != NULL){
-            temp = temp->ptrs[get_i(substr[i])];
-            flag = 1;
-        }
-        else {flag == 0;break;}
-    }
-    return flag;
-}
-        
+       
 void do_dfs(struct Node **cnode,char *path)
 {
-    //DEBUG: 
     int i,j,cur_posn;
     
     char *temp = (char *)malloc(MAX_LEN*sizeof(char));
@@ -86,10 +76,7 @@ void init_node(struct Node** root)
     for(i=0;i<MAX_ALPHA;i++)
         (*root)->ptrs[i] = NULL;
 }
-void alloc_node(struct Node** to_alloc)
-{
-    (*to_alloc) = (struct Node*) malloc(sizeof(struct Node));
-}
+
 struct Node* path_exists(struct Node** chk_node, int index)
 {
     if((*chk_node)->ptrs[index] !=NULL){
@@ -98,7 +85,6 @@ struct Node* path_exists(struct Node** chk_node, int index)
     else{
             // create new node
             struct Node* new_node;
-            //alloc_node(&new_node);
             init_node(&new_node);
             (*chk_node)->ptrs[index] = new_node;
             return new_node;
@@ -120,9 +106,10 @@ int stree_search(struct Node **root, char *q)
     struct Node *temp = *root;
     for(i=0;i<strlen(q);i++)
     {
-       if(temp->ptrs[get_i(q[i])] != NULL)
-            temp = temp->ptrs[get_i(q[i])];
-        else return 0;
+       if(temp->ptrs[get_i(q[i])] == NULL)
+            return 0;
+        temp = temp->ptrs[get_i(q[i])];
+    
     }
-    return is_terminal_str(&temp);
+    return 1;
 }
